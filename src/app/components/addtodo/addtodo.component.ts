e131deeb8b3a12a36015d4974aaea2e628810fbd
harjindergill <laddivaliant@live.com>
2019-12-29 14:24:49 -0800

    angular practice app is complete

---
+++ src/app/components/addtodo/addtodo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-addtodo',
+  templateUrl: './addtodo.component.html',
+  styleUrls: ['./addtodo.component.css']
+})
+export class AddtodoComponent implements OnInit {
+
+@Output() addTodo: EventEmitter<any> = new EventEmitter();
+
+  title:string;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+    const todo = {
+      title: this.title,
+      component: false
+    }
+
+    this.addTodo.emit(todo);
+  }
+
+}
